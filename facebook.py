1e6949e25ba4b1394b7e98f3ebe1c8e5e52f8750
Sriram <sriru1998@gmail.com>
2016-09-24 00:29:52 +0530

    Removed personal stuff.

--- facebook.py
+++ facebook.py
@@ -2,24 +2,21 @@
 # ~digi0ps~ 
 times = 500;
 text = ["Bro", "Bruh", "Dude", "xD", "<3", ":D"]
-smileys = [":)", ":D", ":P", "<3", "^_^", ":3"]
-username = {"Ajay": 'Ajaaaaaaaay',"Ambika":"theprettyprincessforever", "Harinee": "Harinee.morkonda"}; #optional
 import time
 from random import randint as r
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys as k
 chrome = webdriver.Chrome();
 chrome.get("https://www.facebook.com");
-chrome.find_element_by_id("email").send_keys("");
-chrome.find_element_by_id("pass").send_keys("");
+chrome.find_element_by_id("email").send_keys(input("Username: "));
+chrome.find_element_by_id("pass").send_keys(input("Password: "));
 time.sleep(1);
 chrome.find_element_by_id("pass").send_keys(k.ENTER);
-chrome.get("https://www.facebook.com/messages/" + "Irudhu");
+chrome.get("https://www.facebook.com/messages/" + "username");
 chat = chrome.find_elements_by_class_name("_1rv")[0];
 
 for x in range(1, times+1):
 	#chat.send_keys("Square of " + str(x) +" : " + str(x**2));
 	#chat.send_keys("Hi " + text[r(0,len(text)-1)]);
 	#chat.send_keys(str(x)+": Go eat");
-	chat.send_keys("I love you.");
 	chat.send_keys(k.ENTER);
